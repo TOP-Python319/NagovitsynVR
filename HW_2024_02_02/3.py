196bf96ea640249c85ad7cd58cd7a4a863cf604d
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-03-08 15:28:15 +0400

    Решение первого ДЗ

---
+++ HW_2024_02_02/3.py
@@ -0,0 +1,5 @@
+number = int(input())
+print(f"{number} мин - это  {number // 60} час {number % 60}  мин")
+
+#150
+#150 мин  - это 2  час 30  мин
---
+++ HW_2024_02_02/5.py
@@ -0,0 +1,8 @@
+dist1 = int(input())
+dist2 = int(input())
+num = dist1 + (dist2 / 10)
+print(f"{num} миль = {(num*1.61):.1f} км")
+
+#15
+#7
+#15.7 миль = 25.3 км
