d3530be405a10ad37b0f9437cdcb287b23082c05
kwosku <162344822+kwosku@users.noreply.github.com>
2024-04-02 12:49:36 +0300

    4.py

---
+++ HW_2024_02_20/4.py
@@ -0,0 +1,20 @@
+def counter(n):
+    count = 0
+    if n == 1:
+        count = 4
+    elif n > 1:
+        last_number = int('9'*n)
+        first_number = int('1'*n) - int('1'*(n-1))
+        
+        for i in range(first_number,last_number+1):
+            if i % 2 > 0 and i % 3 > 0 and i % 5>0 and i % 7>0:
+                count += 1
+    return count
+            
+    
+
+print(counter(1))
+print(counter(3))
+
+# 4
+# 206
